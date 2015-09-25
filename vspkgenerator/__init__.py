2fc16280ce9bc21a7de590f7aa46bacabc84e29c
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-09-25 11:36:19 -0700

    use new commands

--- vspkgenerator/__init__.py
+++ vspkgenerator/__init__.py
@@ -9,18 +9,18 @@ def generate_vspk(argv=sys.argv):
     """
     """
     path = pkg_resources.resource_filename('vspkgenerator', 'conf/config.ini')
-    subprocess.call(["generate-sdk", "--config", path] + argv[1:])
+    subprocess.call(["monogen-sdk", "--config", path] + argv[1:])
 
 
 def generate_vspkdoc(argv=sys.argv):
     """
     """
     path = pkg_resources.resource_filename('vspkgenerator', 'conf/config.ini')
-    subprocess.call(["generate-sdkdoc", "--config", path] + argv[1:])
+    subprocess.call(["monogen-sdkdoc", "--config", path] + argv[1:])
 
 
 def generate_vsd_apidoc(argv=sys.argv):
     """
     """
     path = pkg_resources.resource_filename('vspkgenerator', 'conf/config.ini')
-    subprocess.call(["generate-apidoc", "--config", path] + argv[1:])
+    subprocess.call(["monogen-apidoc", "--config", path] + argv[1:])
