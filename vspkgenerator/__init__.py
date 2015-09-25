d8acc590c55527f64e10dc391b28cce21e4e6b15
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-09-25 11:27:00 -0700

    update

--- vspkgenerator/__init__.py
+++ vspkgenerator/__init__.py
@@ -8,19 +8,19 @@ import sys
 def generate_vspk(argv=sys.argv):
     """
     """
-    path = pkg_resources.resource_filename('vspkgenerator', 'vanilla/config.ini')
+    path = pkg_resources.resource_filename('vspkgenerator', 'conf/config.ini')
     subprocess.call(["generate-sdk", "--config", path] + argv[1:])
 
 
 def generate_vspkdoc(argv=sys.argv):
     """
     """
-    path = pkg_resources.resource_filename('vspkgenerator', 'vanilla/config.ini')
+    path = pkg_resources.resource_filename('vspkgenerator', 'conf/config.ini')
     subprocess.call(["generate-sdkdoc", "--config", path] + argv[1:])
 
 
 def generate_vsd_apidoc(argv=sys.argv):
     """
     """
-    path = pkg_resources.resource_filename('vspkgenerator', 'vanilla/config.ini')
+    path = pkg_resources.resource_filename('vspkgenerator', 'conf/config.ini')
     subprocess.call(["generate-apidoc", "--config", path] + argv[1:])
