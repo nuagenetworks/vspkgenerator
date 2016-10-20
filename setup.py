f7f7a24791cc4c528d4c1e5fb936a9e33d1c005d
Corentin Henry <corentinhenry@gmail.com>
2016-10-20 09:09:04 -0700

    bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     name='vspkgenerator',
     packages=find_packages(exclude=['*tests*']),
     include_package_data=True,
-    version='1.1.0',
+    version='1.1.1',
     description='VSPK Generator',
     author='Christophe Serafin',
     author_email='christophe.serafin@nuagenetworks.net',
