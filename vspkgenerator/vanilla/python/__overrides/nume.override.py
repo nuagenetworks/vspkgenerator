915efd94b3530f5f104b9cd5f74519d75037f540
Philippe Dellaert <philippe@dellaert.org>
2019-05-28 11:42:24 +0200

    Improving backward compatible solution for async parameter

--- vspkgenerator/vanilla/python/__overrides/nume.override.py
+++ vspkgenerator/vanilla/python/__overrides/nume.override.py
@@ -1,3 +1,5 @@
 def save(self, as_async=False, callback=None, **kwargs):
     """ """
-    super(NUMe, self).save(as_async=as_async, callback=callback, encrypted=False, **kwargs)
+    if 'async' in kwargs.keys() and not as_async:
+        as_async = kwargs['async']
+    super(NUMe, self).save(as_async=as_async, callback=callback, encrypted=False)
