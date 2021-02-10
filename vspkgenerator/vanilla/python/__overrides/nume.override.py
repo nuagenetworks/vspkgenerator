ec685fb1720460c560d08e002c62cc50f2246a95
Rajesh Babu M M <infra@wfnubuildpc07.wf.nuagenetworks.net>
2021-02-10 17:29:29 -0500

    Convert to python3 using futurize

--- vspkgenerator/vanilla/python/__overrides/nume.override.py
+++ vspkgenerator/vanilla/python/__overrides/nume.override.py
@@ -1,5 +1,5 @@
 def save(self, as_async=False, callback=None, **kwargs):
     """ """
-    if 'async' in kwargs.keys() and not as_async:
+    if 'async' in list(kwargs.keys()) and not as_async:
         as_async = kwargs['async']
     super(NUMe, self).save(as_async=as_async, callback=callback, encrypted=False)
